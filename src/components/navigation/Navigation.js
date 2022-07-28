cbc6fb5a0c0142854f80df44b6c70e41bdc644ed
unknown <51543682+graffkevin@users.noreply.github.com>
2022-07-28 13:59:33 +0200

    Update Navigation.js

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -10,20 +10,19 @@ function NavigationTool() {
         <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand as={Link} to="/home">Home</Navbar.Brand>
+                    
+
                 <Nav className="me-auto">
+                    <NavDropdown title="Experiences" id="nav-dropdown">
+                        <NavDropdown.Item eventKey="4.1" as={Link} to="/thesis">Thèse de doctorat</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="4.2" as={Link} to="/cci-idf">CCI-IDF</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="4.3" as={Link} to="/coexya">Coexya</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item eventKey="4.4">Appenin</NavDropdown.Item>
+                    </NavDropdown>
                     <Nav.Link as={Link} to="/parcours">Parcours</Nav.Link>
                     <Nav.Link as={Link} to="/cci-idf">Publication</Nav.Link>
                     <Nav.Link as={Link} to="/coexya">Compétences</Nav.Link>
-                    <a  href="#">
-                        <NavDropdown title="Experiences" id="nav-dropdown">
-                            <NavDropdown.Item eventKey="4.1" as={Link} to="/thesis">Thèse de doctorat</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="4.2" as={Link} to="/cci-idf">CCI-IDF</NavDropdown.Item>
-                            <NavDropdown.Item eventKey="4.3" as={Link} to="/coexya">Coexya</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item eventKey="4.4">Appenin</NavDropdown.Item>
-                        </NavDropdown>
-                    </a>
-                    
                 </Nav>
                 
             </Container>
